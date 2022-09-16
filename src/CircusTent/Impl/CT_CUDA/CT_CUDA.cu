8be12cca55a52ccf3d3c6307a183f533ad545c4a
Pedro <pbbdasilva@gmail.com>
2022-09-16 15:51:06 -0500

    Fix out of bounds problem and copying issue

--- src/CircusTent/Impl/CT_CUDA/CT_CUDA.cu
+++ src/CircusTent/Impl/CT_CUDA/CT_CUDA.cu
@@ -1,6 +1,5 @@
 /*
- * FIXME: ensure proper file exension is used throughout this file
- * _CT_CUDA_CU
+ * _CT_CUDA_CU_
  *
  * Copyright (C) 2017-2021 Tactical Computing Laboratories, LLC
  * All Rights Reserved
@@ -12,9 +11,6 @@
 #include "CT_CUDA.cuh"
 #ifdef _CT_CUDA_CUH_
 
-
-
-
 // Kernels meant to be run on the device
 // __global__  void RAND_ADD(
 //     uint64_t *ARRAY,
@@ -22,7 +18,6 @@
 //     uint64_t iters,
 //     uint64_t pes
 // ) {
-//     // TODO: RAND_ADD()
 // }
 
 CT_CUDA::CT_CUDA(CTBaseImpl::CTBenchType B, CTBaseImpl::CTAtomType A) :
@@ -36,8 +31,8 @@ CT_CUDA::CT_CUDA(CTBaseImpl::CTBenchType B, CTBaseImpl::CTAtomType A) :
     iters(0),
     elems(0),
     stride(0),
-    // TODO: make sure these are all set by the user
     deviceID(-1),
+    deviceCount(0),
     blocksPerGrid(-1),
     threadsPerBlock(-1)
     {}
@@ -45,9 +40,34 @@ CT_CUDA::CT_CUDA(CTBaseImpl::CTBenchType B, CTBaseImpl::CTAtomType A) :
 CT_CUDA::~CT_CUDA() {}
 
 // helper functions
-bool CT_CUDA::parseCUDAOpts(int argc, char **argv) {
-    // CTOpts::l_argc = argc;
-    // CTOpts::l_argv = argv;
+bool CT_CUDA::PrintCUDADeviceProperties(int deviceID, int deviceCount) {
+    cudaGetDeviceCount(&deviceCount);
+    std::cout << "\nCT_CUDA::printCUDADeviceProperties : Number of CUDA enabled devices detected: " << deviceCount << std::endl;
+
+    if (getenv("CUDA_VISIBLE_DEVICES") == nullptr) {
+        std::cout << "CT_CUDA::printCUDADeviceProperties : CUDA_VISIBLE_DEVICES environment variable not set, defaulting to cudaSetDevice(1)" << std::endl;
+        deviceID = cudaSetDevice(1);
+    }
+
+    if (!deviceID && getenv("CUDA_VISIBLE_DEVICES") == nullptr) {
+        std::cout << "CT_CUDA::printCUDADeviceProperties : No target devices detected!" << std::endl;
+        return false;
+    }
+    else {
+        cudaDeviceProp prop;
+        cudaGetDeviceProperties(&prop, deviceID);
+
+        std::cout << "CT_CUDA::printCUDADeviceProperties : Target CUDA deviceID : " << deviceID << std::endl;
+        std::cout << "CT_CUDA::printCUDADeviceProperties : Device Name: " << prop.name << std::endl;
+        std::cout << "CT_CUDA::printCUDADeviceProperties : Memory Clock Rate (KHz): " << prop.memoryClockRate << std::endl;
+        std::cout << "CT_CUDA::printCUDADeviceProperties : Memory Bus Width (bits): " << prop.memoryBusWidth << std::endl;
+        // FIXME: std::cout << "CT_CUDA::printCUDADeviceProperties : Peak Memory Bandwidth (GB/s): " << (2.0 * prop.memoryClockRate * (prop.memoryBusWidth / 8) / 1000000) << std:endl;
+    }
+
+    return true;
+}
+
+bool CT_CUDA::ParseCUDAOpts(int argc, char **argv) { // FIXME:
     for (int i=1; i < argc; i++) {
         std::string s(argv[i]);
 
@@ -69,6 +89,7 @@ bool CT_CUDA::parseCUDAOpts(int argc, char **argv) {
             threadsPerBlock = atoi(P.c_str());
             i++;
         }
+        // TODO: add option to print CUDA device info without having to execute a kernel
     }
 
     // sanity check the options
@@ -85,10 +106,6 @@ bool CT_CUDA::parseCUDAOpts(int argc, char **argv) {
     return true;
 }
 
-// void CT_CUDA::printCUDADeviceProperties(int deviceID) { // TODO: printDeviceProperties()
-
-// }
-
 bool CT_CUDA::AllocateData(uint64_t m, uint64_t p, uint64_t i, uint64_t s) {
     // save the data
     memSize = m;
@@ -98,7 +115,7 @@ bool CT_CUDA::AllocateData(uint64_t m, uint64_t p, uint64_t i, uint64_t s) {
 
     // check args
     if ( pes == 0 ) {
-        std::cout << "CT_CUDA::AllocateData: `pes` cannot be 0" << std::endl; // FIXME: do we need pes for CUDA?
+        std::cout << "CT_CUDA::AllocateData: `pes` cannot be 0" << std::endl;
         return false;
     }
     if ( iters == 0 ) {
@@ -132,19 +149,20 @@ bool CT_CUDA::AllocateData(uint64_t m, uint64_t p, uint64_t i, uint64_t s) {
     Idx = (uint64_t *) malloc(memSize);
     if ( Idx == nullptr ) {
         std::cout << "CT_CUDA::AllocateData : 'Idx' could not be allocated" << std::endl;
+        free(Array);
         free(Idx);
         return false;
     }
 
     // Randomize the arrays on the host
     srand(time(NULL));
-    if ( this->GetBenchType() == CT_PTRCHASE ) {
+    if ( this->GetBenchType() == CT_PTRCHASE ) { // FIXME: ptrchase looks clunky
         for ( unsigned i = 0; i < ((pes+1) * iters); i++ ) {
             Idx[i] = (uint64_t)(rand()%((pes+1)*iters));
         }
     }
     else {
-        for ( unsigned i = 0; i < ((pes+1) * iters); i++ ) {
+        for ( unsigned i = 0; i < elems; i++ ) {
             Idx[i] = (uint64_t)(rand()%(elems-1));
         }
     }
@@ -153,33 +171,223 @@ bool CT_CUDA::AllocateData(uint64_t m, uint64_t p, uint64_t i, uint64_t s) {
     }
 
     // FIXME: allocate data on the target device
-    // TODO: Use cudaSuccess to check that this process is successful, otherwise print an error message
-    cudaMalloc(&d_Array, memSize);
-    cudaMalloc(&d_Idx, memSize); 
+    if ( cudaMalloc(&d_Array, memSize) != cudaSuccess ) {
+        std::cout << "CT_CUDA::AllocateData : 'd_Array' could not be allocated on device" << std::endl;
+        cudaFree(d_Array);
+        free(Array);
+        free(Idx);
+        return false;
+    } // cudaMalloc(&d_Array, memSize);
+
+    if ( cudaMalloc(&d_Idx, 2 * memSize) != cudaSuccess ) {
+        std::cout << "CT_CUDA::AllocateData : 'd_Idx' could not be alloced on device" << std::endl;
+        cudaFree(d_Array);
+        cudaFree(d_Idx);
+        free(Array);
+        free(Idx);
+        return false;
+    } // cudaMalloc(&d_Idx, memSize); 
 
 
     // FIXME: copy arrays from host to target device
-    // TODO: Use cudaSuccess to check that this process is successful, otherwise print an error message
-    // cudaMemcpy(d_Array, &Array, memSize, cudaMemcpyHostToDevice);
-    // cudaMemcpy(d_Idx, &Idx, sizeof(uint64_t)*(pes+1)*iters, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_Array, Array, memSize, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_Idx, Idx, sizeof(uint64_t)*(pes+1)*iters, cudaMemcpyHostToDevice);
+    if ( cudaMemcpy(d_Array, Array, memSize, cudaMemcpyHostToDevice) != cudaSuccess ) {
+        std::cout << "CT_CUDA::AllocateData : 'd_Array' could not be copied to device" << std::endl;
+        cudaFree(d_Array);
+        cudaFree(d_Idx);
+        free(Array);
+        free(Idx);
+        return false;
+    } // cudaMemcpy(d_Array, &Array, memSize, cudaMemcpyHostToDevice);
+
+
+    if ( cudaMemcpy(d_Idx, Idx, 2 * memSize, cudaMemcpyHostToDevice) != cudaSuccess ) {
+        std::cout << "CT_CUDA::AllocateData : 'd_Idx' could not be copied to device" << std::endl;
+        cudaFree(d_Array);
+        cudaFree(d_Idx);
+        free(Array);
+        free(Idx);
+        return false;
+    } // cudaMemcpy(d_Idx, &Idx, sizeof(uint64_t)*(pes+1)*iters, cudaMemcpyHostToDevice);
 
     return true;
 }
 
-bool CT_CUDA::Execute(double &Timing, double &GAMS) { // TODO: CT_CUDA::Execute()
+bool CT_CUDA::Execute(double &Timing, double &GAMS) {
 
     CTBaseImpl::CTBenchType BType   = this->GetBenchType(); // benchmark type
     CTBaseImpl::CTAtomType  AType   = this->GetAtomType();  // atomic type
+    double StartTime = 0.; // start time
+    double EndTime   = 0.; // end time
+    double OPS       = 0.; // billions of operations
 
-    // double StartTime = 0.; // start time
-    // double Endtime   = 0.; // end time
-    // double OPS       = 0.; // billions of operations
-
-    // TODO: determine benchmark type and launch the desired kernel
-
-
+    // determine benchmark type and launch the desired kernel
+    if ( BType == CT_RAND ) {
+        switch ( AType ) {
+            case CT_ADD:
+                StartTime = this->MySecond();
+                RAND_ADD<<< blocksPerGrid, threadsPerBlock >>>( d_Array, d_Idx, iters, pes );
+                EndTime   = this->MySecond();
+                OPS = this->GAM(1, iters, pes);
+                break;
+            case CT_CAS:
+                StartTime = this->MySecond();
+                RAND_CAS<<< blocksPerGrid, threadsPerBlock >>>( d_Array, d_Idx, iters, pes );
+                EndTime   = this->MySecond();
+                OPS = this->GAM(1, iters, pes);
+                break;
+            default:
+                this->ReportBenchError();
+                return false;
+                break;
+        }
+    }
+    else if ( BType == CT_STRIDE1 ) {
+        switch( AType ) {
+            case CT_ADD:
+                StartTime = this->MySecond();
+                STRIDE1_ADD<<< blocksPerGrid, threadsPerBlock >>>( d_Array, iters, pes );
+                EndTime   = this->MySecond();
+                OPS = this->GAM(1, iters, pes);
+                break;
+            case CT_CAS:
+                StartTime = this->MySecond();
+                STRIDE1_CAS<<< blocksPerGrid, threadsPerBlock >>>( d_Array, iters, pes );
+                EndTime   = this->MySecond();
+                OPS = this->GAM(1, iters, pes);
+                break;
+            default:
+                this->ReportBenchError();
+                return false;
+                break;
+        }
+    }
+    else if ( BType == CT_STRIDEN ) {
+        switch( AType ) {
+            case CT_ADD:
+                StartTime = this->MySecond();
+                STRIDEN_ADD<<< blocksPerGrid, threadsPerBlock >>>( d_Array, d_Idx, iters, pes, stride );
+                EndTime   = this->MySecond();
+                OPS = this->GAM(1, iters, pes);
+                break;
+            case CT_CAS:
+                StartTime = this->MySecond();
+                STRIDEN_CAS<<< blocksPerGrid, threadsPerBlock >>>( d_Array, d_Idx, iters, pes, stride );
+                EndTime   = this->MySecond();
+                OPS = this->GAM(1, iters, pes);
+                break;
+            default:
+                this->ReportBenchError();
+                return false;
+                break;
+        }
+    }
+    else if ( BType == CT_PTRCHASE ) {
+        switch( AType ) {
+            case CT_ADD:
+                StartTime = this->MySecond();
+                PTRCHASE_ADD<<< blocksPerGrid, threadsPerBlock >>>( d_Array, d_Idx, iters, pes );
+                EndTime   = this->MySecond();
+                OPS = this->GAM(1, iters, pes);
+                break;
+            case CT_CAS:
+                StartTime = this->MySecond();
+                PTRCHASE_CAS<<< blocksPerGrid, threadsPerBlock >>>( d_Array, d_Idx, iters, pes );
+                EndTime   = this->MySecond();
+                OPS = this->GAM(1, iters, pes);
+                break;
+            default:
+                this->ReportBenchError();
+                return false;
+                break;
+        }
+    }
+    else if ( BType == CT_SG ) {
+        switch( AType ) {
+            case CT_ADD:
+                StartTime = this->MySecond();
+                SG_ADD<<< blocksPerGrid, threadsPerBlock >>>( d_Array, d_Idx, iters, pes );
+                EndTime   = this->MySecond();
+                OPS = this->GAM(4, iters, pes);
+                break;
+            case CT_CAS:
+                StartTime = this->MySecond();
+                SG_CAS<<< blocksPerGrid, threadsPerBlock >>>( d_Array, d_Idx, iters, pes );
+                EndTime   = this->MySecond();
+                OPS = this->GAM(4, iters, pes);
+                break;
+            default:
+                this->ReportBenchError();
+                return false;
+                break;
+        }
+    }
+    else if ( BType == CT_CENTRAL ) {
+        switch( AType ) {
+            case CT_ADD:
+                StartTime = this->MySecond();
+                CENTRAL_ADD<<< blocksPerGrid, threadsPerBlock >>>( d_Array, d_Idx, iters, pes );
+                EndTime   = this->MySecond();
+                OPS = this->GAM(1, iters, pes);
+                break;
+            case CT_CAS:
+                StartTime = this->MySecond();
+                CENTRAL_CAS<<< blocksPerGrid, threadsPerBlock >>>( d_Array, d_Idx, iters, pes );
+                EndTime   = this->MySecond();
+                OPS = this->GAM(1, iters, pes);
+                break;
+            default:
+                this->ReportBenchError();
+                return false;
+                break;
+        }
+    }
+    else if ( BType == CT_SCATTER ) {
+        switch( AType ) {
+            case CT_ADD:
+                StartTime = this->MySecond();
+                SCATTER_ADD<<< blocksPerGrid, threadsPerBlock >>>( d_Array, d_Idx, iters, pes );
+                EndTime   = this->MySecond();
+                OPS = this->GAM(3, iters, pes);
+                break;
+            case CT_CAS:
+                StartTime = this->MySecond();
+                SCATTER_CAS<<< blocksPerGrid, threadsPerBlock >>>( d_Array, d_Idx, iters, pes );
+                EndTime   = this->MySecond();
+                OPS = this->GAM(3, iters, pes);
+                break;
+            default:
+                this->ReportBenchError();
+                return false;
+                break;
+        }
+    }
+    else if ( BType == CT_GATHER ) {
+        switch( AType ) {
+            case CT_ADD:
+                StartTime = this->MySecond();
+                GATHER_ADD<<< blocksPerGrid, threadsPerBlock >>>( d_Array, d_Idx, iters, pes );
+                EndTime   = this->MySecond();
+                OPS = this->GAM(3, iters, pes);
+                break;
+            case CT_CAS:
+                StartTime = this->MySecond();
+                GATHER_CAS<<< blocksPerGrid, threadsPerBlock >>>( d_Array, d_Idx, iters, pes );
+                EndTime   = this->MySecond();
+                OPS = this->GAM(3, iters, pes);
+                break;
+            default:
+                this->ReportBenchError();
+                return false;
+                break;
+        }
+    }
+    else {
+        this->ReportBenchError();
+        return false;
+    }
+    
+    Timing = this->Runtime(StartTime,EndTime);
+    GAMS   = OPS/Timing;
 
     return true;
 }
