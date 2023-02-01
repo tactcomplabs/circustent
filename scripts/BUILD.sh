5d6cdc2eef8fe555c95b0c40878fbc1658c18d1f
Michael Beebe <mibeebe@login-20-26.localdomain>
2023-01-31 19:21:44 -0600

    still trying different flags to get OMP and OACC impls to ship to NVIDIA GPU

--- scripts/BUILD.sh
+++ scripts/BUILD.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+##################################################
 # Flags for each GPU impl
+##################################################
 CMAKE_CUDA_FLAGS="-DENABLE_CUDA=ON"
-# CMAKE_OMP_FLAGS="-fopenmp -fopenmp-targets=nvptx64-nvidia-cuda -Xopenmp-target -march=sm_70 -DENABLE_OMP_TARGET=ON"
-CMAKE_OMP_FLAGS="-DENABLE_OMP_TARGET=ON"
-CMAKE_OACC_FLAGS="-acc -ta=tesla:cuda -Minfo=accel -gpu=cc70 -DENABLE_OPENACC=ON"
+
+CMAKE_OMP_FLAGS="-fopenmp -fopenmp-targets=nvptx64-nvidia-cuda \\
+    -⁠mp=gpu -Xopenmp-target -march=sm_70 -gpu=cc70,cuda11.0 \\
+    -foffload=nvptx-none -DENABLE_OMP_TARGET=ON"
+
+CMAKE_OACC_FLAGS="-arch=sm_75 -std=c++14 -Xcompiler -fopenacc -ta=tesla:cuda \\
+    -Minfo=accel -gpu=cc70 -DENABLE_OPENACC=ON"
 
 
 # Set flags for desired backend here
 CMAKE_FLAGS=$CMAKE_OACC_FLAGS
-
 
 ##################################################
 # Don't edit below
--- scripts/nv_v100/add_kernels/add_kernels.sh
+++ scripts/nv_v100/add_kernels/add_kernels.sh
@@ -4,33 +4,47 @@
 
 HLINE="============================================================="
 
-BACKENDS="CUDA OpenMP OpenACC"
+#######################################################################
+#           Set the implementation that you wish to run                      
+#######################################################################
+BACKENDS=("CUDA" "OpenMP" "OpenACC")
 
-# Set subscript to 0 for CUDA, 1 for OpenMP, 2 for OpenACC
-# IMPL=$BACKENDS[0]
+# IMPL=$BACKENDS[3]
 IMPL="OpenACC"
 
 #######################################################################
-
-if [[ "$IMPL" == "CUDA" ]]; then
+#               Set implementation-specific env variables
+#######################################################################
+if [[ "$IMPL" == "OpenMP" ]]; then
     BLOCKS=16
-    THREADS=128
-elif [[ "$IMPL" == "OpenMP" ]]; then
-    export OMP_DEFAULT_DEVICE=
+    export OMP_DEFAULT_DEVICE=0
     export OMP_NUM_THREADS=128
 elif [[ "$IMPL" == "OpenACC" ]]; then
     export ACC_DEVICE_TYPE=nvidia
     export ACC_DEVICE_NUM=0
 fi
 
-
 #######################################################################
-
+#                 Edit the run run configuration
+#######################################################################
+BLOCKS=16
+THREADS=128
 MEM_SIZE=32089730048
 ITERS=500000
 
 #######################################################################
+#     Set $COMPILE to true if you want to run the build script
+#######################################################################
+COMPILE=
+if [[ $COMPILE == true ]]; then
+    cd ../../
+    source BUILD.sh
+    cd nv_v100/add_kernels
+fi
 
+#######################################################################
+#                      Create output file  
+#######################################################################
 TEST_DIR=$(pwd)
 OUTPUT_FILE=$TEST_DIR/outputs/$IMPL/add_kernels_$(date +"%d-%m-%y")_$(date +"%T").txt
 if [[ -f $OUTPUT_FILE ]] ; then
@@ -40,6 +54,9 @@ else
     touch $OUTPUT_FILE
 fi
 
+#######################################################################
+#             Specify path to the circustent executable 
+#######################################################################
 cd ../../../
 CT_ROOT=$(pwd)
 CT_BUILD=$CT_ROOT/build
@@ -48,27 +65,28 @@ cd $TEST_DIR
 
 
 #######################################################################
-
+#                         Run each add kernel
+#######################################################################
 BENCH="RAND_ADD STRIDE1_ADD PTRCHASE_ADD CENTRAL_ADD SG_ADD SCATTER_ADD GATHER_ADD" 
 for B in $BENCH; do
-    echo $HLINE >> $OUTPUT_FILE
-    echo "  Running the $B kernel using the $IMPL impl..." >> $OUTPUT_FILE
-    echo "                  $(date +"%d-%m-%y") AT $(date +"%T") " >> $OUTPUT_FILE
-    echo $HLINE >> $OUTPUT_FILE
+    echo $HLINE | tee $OUTPUT_FILE
+    echo "  Running the $B kernel using the $IMPL impl..." | tee $OUTPUT_FILE
+    echo "                  $(date +"%d-%m-%y") AT $(date +"%T") " | tee $OUTPUT_FILE
+    echo $HLINE | tee $OUTPUT_FILE
 
     if [[ "$IMPL" == "CUDA" ]]; then    
-        $EXE --bench $B -m $MEM_SIZE -i $ITERS --blocks $BLOCKS --threads $THREADS >> $OUTPUT_FILE
-        echo >> $OUTPUT_FILE
-        echo >> $OUTPUT_FILE
+        $EXE --bench $B -m $MEM_SIZE -i $ITERS --blocks $BLOCKS --threads $THREADS | tee $OUTPUT_FILE
+        echo | tee $OUTPUT_FILE
+        echo | tee $OUTPUT_FILE
     elif [[ "$IMPL" == "OpenMP" ]]; then
         # FIXME: Is OpenMP teams == CUDA blocks? -p is the teams
-        $EXE --bench $B -m $MEM_SIZE -p $BLOCKS -i $ITERS >> $OUTPUT_FILE        
-        echo >> $OUTPUT_FILE
-        echo >> $OUTPUT_FILE
+        $EXE --bench $B -m $MEM_SIZE -p $BLOCKS -i $ITERS | tee $OUTPUT_FILE        
+        echo | tee $OUTPUT_FILE
+        echo | tee $OUTPUT_FILE
     elif [[ "$IMPL" == "OpenACC" ]]; then
         # FIXME: Is OpenACC gangs == CUDA blocks? -p is the teams 
-        $EXE --bench $B -m $MEM_SIZE -p $BLOCKS -i $ITERS >> $OUTPUT_FILE        
-        echo >> $OUTPUT_FILE
-        echo >> $OUTPUT_FILE 
+        $EXE --bench $B -m $MEM_SIZE -p $BLOCKS -i $ITERS | tee $OUTPUT_FILE        
+        echo | tee $OUTPUT_FILE
+        echo | tee $OUTPUT_FILE 
     fi
 done
